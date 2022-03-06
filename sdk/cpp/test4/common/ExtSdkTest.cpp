a3f9a8e3e409a5267fb25381442faf1f36739953
yu.zuo <asterisk@hotmail.com>
2022-03-06 12:39:40 +0800

    update

---
+++ sdk/cpp/test4/common/ExtSdkTest.cpp
@@ -0,0 +1,7 @@
+//
+// Created by 佐玉 on 2022/3/6.
+//
+
+#include "ExtSdkTest.h"
+#include "ExtSdkObject.h"
+#include "ExtSdkLog.h"
---
+++ sdk/cpp/test4/common/ExtSdkTest.h
@@ -0,0 +1,8 @@
+//
+// Created by 佐玉 on 2022/3/6.
+//
+
+#ifndef ANDROID_EXTSDKTEST_H
+#define ANDROID_EXTSDKTEST_H
+
+#endif //ANDROID_EXTSDKTEST_H
